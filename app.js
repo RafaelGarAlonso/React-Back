85fe147abdf165815649a862446e5ad258444158
Rafael <rafaelinfinia@MacBook-Pro-de-Rafael.local>
2018-11-02 18:57:55 +0100

    Configuracion Express con MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Inicializar variables
 var app = express();
+
+//Conexion BBDD
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res )=>{
+  if(err) throw err;
+  console.log('BBDD: \x1b[32m%s\x1b[0m', 'online')
+});
+
+// Rutas
+app.get('/', ( req, res, next ) => {
+  res.status(200).json({
+    ok:true,
+    mensaje:'Petición realizada correctamente'
+  })
+})
 
 // Escuchar peticiones
 app.listen(3000, ()=>{
