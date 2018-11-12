1634996148a5005ff2f1dd6dc52d90c3fd83b7a3
Rafael <rafaelinfinia@MacBook-Pro-de-Rafael.local>
2018-11-12 18:52:45 +0100

    Fix Correspondiente a curso Seccion 13

--- routes/usuario.js
+++ routes/usuario.js
@@ -96,7 +96,7 @@ app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 /*=============================================>>>>>
 = Crear un usuario =
 ===============================================>>>>>*/
-app.post('/', mdAutenticacion.verificaToken ,(req,res) =>{
+app.post('/' ,(req,res) =>{
   var body = req.body;
 
   var usuario = new Usuario({
